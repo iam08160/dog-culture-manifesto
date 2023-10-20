4de97c84091d36577e13f80580fc871ffa65946f
Ben <71155331+bchussid@users.noreply.github.com>
2023-10-20 15:43:46 -0400

    mongo connection bug test

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const app = express();
 
 const mongoose = require("mongoose");
 
-const { PORT } = process.env;
+const { PORT, MONGO } = process.env;
+
+mongoose.connect(`${MONGO}/DogCultureDev`);
+const db = mongoose.connection;
+// db.on('error'), (err) => console.error((err))
+db.once("open", () => console.log("Connected to Database"));
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
